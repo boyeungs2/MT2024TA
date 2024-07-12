3b1085311ff2bbeff543a8ff35f0c22dc6ff10a2
Sunha-i <icecream20@khu.ac.kr>
2024-07-12 17:40:22 +0900

    Add: final controller

--- KartRider/Assets/Scripts/InputManager.cs
+++ KartRider/Assets/Scripts/InputManager.cs
@@ -7,11 +7,13 @@ public class InputManager : MonoBehaviour
     public float vertical;
     public float horizontal;
     public bool handbrake;
+    public bool boosting;
 
     private void FixedUpdate()
     {
         vertical = Input.GetAxis("Vertical");
         horizontal = Input.GetAxis("Horizontal");
         handbrake = (Input.GetAxis("Jump") != 0) ? true : false;
+        boosting = (Input.GetKey(KeyCode.LeftShift)) ? true : false;
     }
 }
--- KartRider/Assets/Scripts/KartController.cs
+++ KartRider/Assets/Scripts/KartController.cs
@@ -4,85 +4,49 @@ using UnityEngine;
 
 public class KartController : MonoBehaviour
 {
-    internal enum driveType {
-        frontWheelDrive,
-        rearWheelDrive,
-        allWheelDrive
-    }
-    [SerializeField] private driveType drive;
-
-    private InputManager IM;
     public WheelCollider[] wheels = new WheelCollider[4];
     public GameObject[] wheelMesh = new GameObject[4];
     public float motorTorque = 100f;
     public float steeringMax = 4f;
 
-    private WheelFrictionCurve forwardFriction, sidewaysFriction;
-
-    void Start()
-    {
-        GetObjects();
-    }
-
     private void FixedUpdate()
     {
-        AnimateWheels();
-        MoveKart();
-        SteerKart();
-    }
+        animateWheels();
 
-    private void MoveKart()
-    {
-        float totalPower;
-
-        if (drive == driveType.allWheelDrive) {
-            for (int i = 0; i < wheels.Length; i++) {
-                wheels[i].motorTorque = IM.vertical * (motorTorque / 4);
+        if (Input.GetKey(KeyCode.W))
+        {
+            for (int i = 0; i < wheels.Length; i++)
+            {
+                wheels[i].motorTorque = motorTorque;
             }
         }
-        else if (drive == driveType.rearWheelDrive) { 
-            for (int i = 2; i < wheels.Length; i++) {
-                wheels[i].motorTorque = IM.vertical * (motorTorque / 2);
-            }
-        }
-        else { 
-            for (int i = 0; i < wheels.Length - 2; i++) {
-                wheels[i].motorTorque = IM.vertical * (motorTorque / 2);
-            }
-        }
-    }
 
-    private void SteerKart()
-    {
-        for (int i = 0; i < wheels.Length - 2; i++) {
-            wheels[i].steerAngle = IM.horizontal * steeringMax;
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            for (int i = 0; i < wheels.Length - 2; i++)
+            {
+                wheels[i].steerAngle = Input.GetAxis("Horizontal") * steeringMax;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < wheels.Length - 2; i++)
+            {
+                wheels[i].steerAngle = 0;
+            }
         }
     }
 
-    void AnimateWheels()
+    void animateWheels()
     {
         Vector3 wheelPosition = Vector3.zero;
         Quaternion wheelRotation = Quaternion.identity;
 
-        for (int i = 0; i < wheels.Length; i++) {
+        for (int i = 0; i < wheels.Length; i++)
+        {
             wheels[i].GetWorldPose(out wheelPosition, out wheelRotation);
             wheelMesh[i].transform.position = wheelPosition;
             wheelMesh[i].transform.rotation = wheelRotation;
         }
     }
-
-    private void GetObjects()
-    {
-        IM = GetComponent<InputManager>();
-    }
-
-    //void adjustTraction()
-    //{
-    //    if (!IM.handbrake) {
-    //        forwardFriction = wheels[0].forwardFriction;
-    //        sidewaysFriction = wheels[0].sidewaysFriction;
-
-    //        forwardFriction.extremumValue = forwardFriction.asymptoteValue = ((currSpeed * frictionM))
-    //    }
-    //}
 }
